342b3213d0baf97f2caf520346c232ce43fcd9ba
shblues68 <shblues68@gmail.com>
2017-09-21 11:26:04 +0300

    ver 1

---
+++ hla_01_Alignments_read.sql
@@ -0,0 +1,25 @@
+-- ****************************************************************************************************
+-- Загрузка данных файлов alignments 
+-- ****************************************************************************************************
+
+-- ****************************************************************************************************
+-- Чтения
+-- ****************************************************************************************************
+	Use DNA_HLA
+
+    -- Прочитать файл    
+    -- Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\A_gen.txt'
+    
+    print '**************************************************'
+    print 'Чтение данных alignments'
+    print '**************************************************'
+    Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\A_gen.txt'
+    Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\B_gen.txt'
+    Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\C_gen.txt'
+    Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\DRB1_gen.txt'
+    Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\DQB1_gen.txt'
+    Exec hla_Alignments_Read @file_name='D:\NGS\DATA\alignments\DPB1_gen.txt'
+    Go
+
+    DBCC SHRINKFILE (N'DNA_HLA_log' , 0, TRUNCATEONLY)
+    GO
